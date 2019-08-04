727bc983e3672e4532020780ff98677e3f45bff9
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-04 12:42:40 -0300

    NEW - Requisição ao servidor NEW - Preparação da store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import './config/reactotronConfig';
+import store from './store';
 
 import Routes from './routes';
 
 const App = () => (
-  <>
+  <Provider store={store}>
     <StatusBar barStyle="light-content" backgroundColor="#000" />
     <Routes />
-  </>
+  </Provider>
 );
 
 export default App;
